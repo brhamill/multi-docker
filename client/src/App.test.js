e9e789aa739ebf0bcd62d17ab83cf04bb7d63258
Brad Hamill <brad@hamill.com>
2019-12-28 14:50:59 -0700

    initial commit

---
+++ client/src/App.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Use mocks when dealing with multiple containers.
+it('renders without crashing', () => {});
